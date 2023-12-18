afea48d73ddb57cafaf8993227f65819d507ddf0
Ronak <ronakpatel5177@gmail.com>
2023-12-18 22:20:22 +0530

    chapter 55 Variables_Obj_Datatype

---
+++ 55_Variables_DataTypes_Objects_JS/script.js
@@ -0,0 +1,32 @@
+console.log("Tutorial 55")
+
+var a = 5;
+var b = 6;
+var c = "Ronak"
+
+console.log(a + b + c)
+console.log(typeof a, typeof b, typeof c)
+
+
+let x = "Dabhi Mayur";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let object1 = {
+    name: "Jigar",
+    "job code": 23298,
+    allocated: true
+
+}
+
+console.log(object1)
+object1.salary = "1Cr"
+console.log(object1)
+object1.salary = "5Cr"
+console.log(object1)
