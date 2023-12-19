68b063c1616f3666cbffe7a758dd35908cf454b5
Ronak <ronakpatel5177@gmail.com>
2023-12-19 21:38:37 +0530

    chapter 60 Strings_JS

---
+++ 60_Strings_JS/index.js
@@ -0,0 +1,32 @@
+let x = "Ronak"
+console.log(x[0])
+console.log(x[1])
+console.log(x[2])
+console.log(x[3])
+console.log(x[4])
+// console.log(x[5])
+
+console.log(x.length)
+
+let real_name = "Mayank"
+let friend = "Mayur"
+
+// console.log("His name is " + real_name + " and his friends name is " + friend)
+console.log(`His name is ${real_name} and his friend name is ${friend}`)
+
+let b = "Jigar"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1, 4))
+console.log(b.slice(1))
+
+let company = "Tata Play"
+let work = "RP IndustriesRP" 
+console.log(company.replace("Play", "Sky"))
+console.log(work.replace("RP", "89"))
+
+console.log(work.concat(" next company will be ", company, " also tata steel"))
+
+let whitespace = "   here are the whitespace   "
+console.log(whitespace.trim())
