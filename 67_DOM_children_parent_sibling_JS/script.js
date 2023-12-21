814fbbb606b242bd259d00d2048038e20ce2beda
Ronak <ronakpatel5177@gmail.com>
2023-12-21 14:55:17 +0530

    chapter 67 DOM update

--- 67_DOM_children_parent_sibling_JS/script.js
+++ 67_DOM_children_parent_sibling_JS/script.js
@@ -13,8 +13,9 @@ console.log(cont.lastElementChild.parentElement)
 // document.body.firstElementChild  //returns container
 // document.body.firstElementChild.childNodes
 
-document.body.firstElementChild.children
+// document.body.firstElementChild.children
 // HTMLCollection(5) [div.box, div.box, div.box, div.box, div.box]
 
-document.body.firstElementChild.children[0]
-document.body.firstElementChild.children[3].nextElementSibling
+// document.body.firstElementChild.children[0]
+// document.body.firstElementChild.children[3].nextElementSibling
+
