09f9b1e01780f7b07e6c08e31c92e6371eaae6de
Ronak <ronakpatel5177@gmail.com>
2024-03-04 08:37:38 +0530

    chapter 108 useEffect

---
+++ 108_useEffect_in_react/useEffect-react/src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import {useEffect} from 'react'
+
+const Navbar = ({color}) => {
+      // Case 1: Run on every render
+  useEffect(()=>{
+    alert("Hello User, I'll run on every render")
+  })
+
+  // Case 2: Run only on first render
+  useEffect(()=>{
+    alert("This will run only on first render")
+  }, [])
+
+//   Case 3: Run only on certain value change
+  useEffect(()=>{
+    alert("Color was updated")
+  }, [color])
+
+
+  // Example of cleanup function
+  useEffect(()=>{
+    return ()=>{
+      alert("Component was unmounted")
+    }
+  }, [])
+  return (
+    <div>
+     This is navbar of {color}
+    </div>
+  )
+}
+
+export default Navbar
